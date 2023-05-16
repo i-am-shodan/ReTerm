94e42af141d9d337f92e4d87c94b82e1a455b80d
i-am-shodan <6901273+i-am-shodan@users.noreply.github.com>
2023-05-16 15:48:30 +0100

    draft config and sln

---
+++ ReTerm.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export DOTNET_SYSTEM_GLOBALIZATION_INVARIANT=1
+export DOTNET_SYSTEM_GLOBALIZATION_PREDEFINED_CULTURES_ONLY=false
+/opt/bin/rm2fb-client /home/root/dotnet/dotnet /home/root/ReTerm/ReTerm.dll
