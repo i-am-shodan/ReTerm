db5342270af69f95bcfe54ea2da77e9214ba8ee3
i-am-shodan <6901273+i-am-shodan@users.noreply.github.com>
2023-06-27 11:32:35 +0100

    Settings support

--- src/ReTerm/Program.cs
+++ src/ReTerm/Program.cs
@@ -8,6 +8,7 @@ using ReMarkable.NET.Unix.Driver.Display.EinkController;
 using ReMarkable.NET.Unix.Driver.Keyboard;
 using ReMarkable.NET.Util;
 using ReTerm.Fonts;
+using ReTerm.Settings;
 using Sandbox.Terminal;
 using SixLabors.Fonts;
 using SixLabors.ImageSharp;
@@ -67,6 +68,8 @@ namespace Sandbox
                 OutputDevices.Display.Draw(blankScreen, blankScreen.Bounds(), Point.Empty, waveformMode: WaveformMode.Auto);
             }
 
+            var settings = TerminalSettings.Get();
+
             // calculate the width of the terminal in chars based on the size of the font
             var terminalWidthInChars = (int)Math.Floor(screen.VisibleHeight / (double)TerminalFont.GetWidth());
             // calculate the height of the terminal in chars based on the height of the font
@@ -90,6 +93,11 @@ namespace Sandbox
 
                     Action<Point> HandleStylusFingerPosition = (point) =>
                     {
+                        if (settings.DisableMouseAndTouch)
+                        {
+                            return;
+                        }
+
                         // We need to convert state.DevicePosition.x and state.DevicePosition.y to the which character
                         // and row the stylus is over. We can then use that to set the cursor position.
                         int x = (screen.VisibleWidth - point.X) / TerminalFont.GetHeight();
@@ -169,14 +177,16 @@ namespace Sandbox
                         VT100DataConsumer.Push(data);
                     };
 
-                    var blinkCursor = Task.Run(async () => { 
+                    // Turn on cursor blinking if configured
+                    var cursorAndMarksActivity = Task.Run(async () =>
+                    {
                         while (!cts.IsCancellationRequested)
                         {
                             var cursor = TerminalController.CursorState;
                             int currentRow = cursor.CurrentRow;
                             int currentCol = cursor.CurrentColumn;
 
-                            foreach (var markToRemove in marks.Where(x => DateTime.Now.Subtract(x.Key.Item1).TotalSeconds > 10).ToArray())
+                            foreach (var markToRemove in marks.Where(x => DateTime.Now.Subtract(x.Key.Item1) > settings.TouchMarksClearTime).ToArray())
                             {
                                 TerminalWindow.UnsetCursor(markToRemove.Key.Item2, markToRemove.Key.Item3);
                                 marks.TryRemove(markToRemove);
@@ -201,9 +211,13 @@ namespace Sandbox
                                         throw new Exception("Unknown cursor");
                                 }
 
-                                TerminalWindow.SetCursor(currentRow, currentCol, cursorChar);
-                                await Task.Delay(500);
-                                TerminalWindow.UnsetCursor(currentRow, currentCol);
+                                // if don't blink is set we update the cursor position once 250ms after an update has occured
+                                if (!settings.DontBlinkCursor)
+                                {
+                                    TerminalWindow.SetCursor(currentRow, currentCol, cursorChar);
+                                    await Task.Delay(settings.CursorBlinkInterval);
+                                    TerminalWindow.UnsetCursor(currentRow, currentCol);
+                                }
                             }
                             await Task.Delay(500, cts.Token);
                         }
